3ddd1f0ebe37d8a46559ff28260a1abfa96de632
Ha Manh Bao <131301257+baohm88@users.noreply.github.com>
2025-11-27 19:39:31 +0700

    1st commit

---
+++ src/main/java/com/example/ead_exmam/EadExmamApplication.java
@@ -0,0 +1,13 @@
+package com.example.ead_exmam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadExmamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EadExmamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ead_exmam/EadExmamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ead_exmam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadExmamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
